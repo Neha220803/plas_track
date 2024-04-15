706272200fcbacb390ca1c1c8f54a0bd8e981618
Neeharika Srini <neeharika.srinivasan@gmail.com>
2024-04-15 16:33:06 +0530

    donate page commit

--- lib/Home/DonatePage.dart
+++ lib/Home/DonatePage.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class DonatePlasticPage extends StatefulWidget {
@@ -16,7 +18,7 @@ class _DonatePlasticPageState extends State<DonatePlasticPage> {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Image.asset(
-            'images/flag.png',
+            "images/flag.png",
             height: 150,
           ),
           const SizedBox(height: 20),
@@ -26,45 +28,90 @@ class _DonatePlasticPageState extends State<DonatePlasticPage> {
             textAlign: TextAlign.center,
           ),
           const SizedBox(height: 20),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              foregroundColor: Colors.white,
-              backgroundColor: Colors.black,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30),
+          Stack(alignment: Alignment.center, children: [
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.white,
+                backgroundColor: Colors.black,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                fixedSize: const Size(300, 50),
               ),
-              fixedSize: const Size(300, 50),
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return Dialog(
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Image.asset('images/success.png', height: 150),
+                            const SizedBox(height: 20),
+                            Text(
+                              "Successful!",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 24,
+                              ),
+                            ),
+                            Text(
+                              "Thank You for Your Contribution!",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 40),
+                              child: Text(
+                                "Your response has been noted for the recycling program. You will receive the appropriate reward when the order reaches the warehouse.",
+                                textAlign: TextAlign.center,
+                                style:
+                                    TextStyle(fontSize: 16, color: Colors.grey),
+                              ),
+                            ),
+                            const SizedBox(height: 20),
+                            TextButton(
+                              onPressed: () => Navigator.pop(context),
+                              child: Text("OK"),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+              child: const Text("Yes, Flag my location"),
             ),
-            onPressed: () {
-              // Navigator.push(
-              //   context,
-              //   MaterialPageRoute(
-              //     builder: (context) => DonatePlastic(),
-              //   ),
-              // );
-            },
-            child: const Text("Yes, Flag my location"),
-          ),
+          ]),
           const SizedBox(height: 10),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              foregroundColor: Colors.white,
-              backgroundColor: Colors.black,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30),
-              ),
-              fixedSize: const Size(300, 50),
-            ),
-            onPressed: () {
-              // Navigator.push(
-              //   context,
-              //   MaterialPageRoute(
-              //     builder: (context) => SuccesLog(),
-              //   ),
-              // );
-            },
-            child: const Text("Return Home"),
-          ),
+          // ElevatedButton(
+          //   style: ElevatedButton.styleFrom(
+          //     foregroundColor: Colors.white,
+          //     backgroundColor: Colors.black,
+          //     shape: RoundedRectangleBorder(
+          //       borderRadius: BorderRadius.circular(30),
+          //     ),
+          //     fixedSize: const Size(300, 50),
+          //   ),
+          //   onPressed: () {
+          //     // Navigator.push(
+          //     //   context,
+          //     //   MaterialPageRoute(
+          //     //     builder: (context) => SuccesLog(),
+          //     //   ),
+          //     // );
+          //   },
+          //   child: const Text("Return Home"),
+          // ),
         ],
       ),
     );
