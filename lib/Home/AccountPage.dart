faa1d87b64220a9289a654f0659ee29cdd55987a
Neeharika Srini <neeharika.srinivasan@gmail.com>
2024-04-15 16:38:40 +0530

    accounts page done

--- lib/Home/AccountPage.dart
+++ lib/Home/AccountPage.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class AccountPage extends StatefulWidget {
@@ -10,6 +12,28 @@ class AccountPage extends StatefulWidget {
 class _AccountPageState extends State<AccountPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Incentives Received',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 8.0),
+          ListTile(
+            title: Text('Recycling Bonus'),
+            subtitle: Text('Received on August 15, 2023'),
+            trailing: Text('+Rs 5.00'),
+          ),
+          ListTile(
+            title: Text('Recycling Bonus'),
+            subtitle: Text('Received on August 10, 2023'),
+            trailing: Text('+Rs. 2.50'),
+          ),
+        ],
+      ),
+    );
   }
 }
